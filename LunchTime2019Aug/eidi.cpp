09e3dcdf65f32403c0d7892e5bc1be520bf7a2cc
yasser <yasser.info.work@gmail.com>
2019-09-03 23:33:40 +0530

    Fixed Problem Eidi!

--- LunchTime2019Aug/eidi.cpp
+++ LunchTime2019Aug/eidi.cpp
@@ -1,43 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 string solnFn(int n1,int n2,int n3,int m1,int m2, int m3){
-    //case1 all same age :
-    if(n1==n2==n3){
-        if(m1==m2==m3) return "FAIR";
-        else return "NOT FAIR";
-    }else if(n1!=n2 && n1!=n3 && n2!=n3){
-        int arr1[] = {n1,n2,n3};
-        int arr2[] = {m1,m2,m3};
-        for(int i =0;i<3;i++){
-            for(int j=0;j<2;j++){
-                if(arr1[j]>arr1[j+1]){
-                    int temp = arr1[j+1];
-                    arr1[j+1] = arr1[j];
-                    arr1[j] = temp;
+    int arr1[] = {n1,n2,n3};
+    int arr2[] = {m1,m2,m3}; // sorting
+    for(int i =0;i<3;i++){
+        for(int j=0;j<2;j++){
+            if(arr1[j]>arr1[j+1]){
+                int temp = arr1[j+1];
+                arr1[j+1] = arr1[j];
+                arr1[j] = temp;
 
-                    int temp2 = arr2[j+1];
-                    arr2[j+1] = arr2[j];
-                    arr2[j] = temp2;
-                }
+                int temp2 = arr2[j+1];
+                arr2[j+1] = arr2[j];
+                arr2[j] = temp2;
             }
         }
-        if(arr2[2]>arr2[1] && arr2[1] > arr2[0]) return "FAIR";
+    }
+    if(arr1[2]>arr1[1] && arr1[1]>arr1[0]){
+        if (arr2[2]>arr2[1] && arr2[1]>arr2[0]) return "FAIR";
         else return "NOT FAIR";
-    }else{
-        if(n1==n2){
-            if(m1==m2) return "FAIR";
-            else return "NOT FAIR";
-        }if(n2==n3){
-            if(m2==m3) return "FAIR";
-            else return "NOT FAIR";
-        }if(n1==n3){
-            if(m1==m3) return "FAIR";
-            else return "NOT FAIR";
-        }
+    }else if(arr1[2] == arr1[1] && arr1[1] == arr1[0]){
+        if(arr2[2] == arr2[1] && arr2[1] == arr2[0]) return "FAIR";
+        else return "NOT FAIR";
+    }else if(arr1[2] == arr1[1] && arr1[1]>arr1[0]){
+        if(arr2[2] == arr2[1] && arr2[1]>arr2[0]) return "FAIR";
+        else return "NOT FAIR";
+    }else if(arr1[1] == arr1[0] && arr1[0]<arr1[2]){
+        if(arr2[1] == arr2[0] && arr2[0]<arr2[2]) return "FAIR";
+        else return "NOT FAIR";
     }
 }
 int main(){
-    int t=1;
+    int t;
     cin>>t;
     while(t--){
         int n1,n2,n3,m1,m2,m3;
